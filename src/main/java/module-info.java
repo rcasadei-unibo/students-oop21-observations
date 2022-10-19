eae8a807e666670ccf60561920e5a255abf424e3
Giovanni Tentelli <giovanni.tentelli@studio.unibo.it>
2022-10-19 15:28:20 +0200

    Added apache.pdfbox to dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module org.observations {
     requires javafx.graphics;
     requires javafx.controls;
     requires org.testng;
+    requires org.apache.pdfbox;
+    requires org.apache.fontbox;
 
     exports org.observations;
     opens org.observations to javafx.graphics;
