6a1cb57127a7fba049211d89b5d22406aa87d99a
Giovanni Tentelli <giovanni.tentelli@studio.unibo.it>
2022-10-21 12:52:05 +0200

    (Hotfix) Added pdf exporter feature

--- src/main/java/org/observations/utility/PdfExporter.java
+++ src/main/java/org/observations/utility/PdfExporter.java
@@ -23,12 +23,18 @@ public class PdfExporter {
     private static final String NAME_APP = "Observations";
     private static final String EXPORT_PATH = ROOT + SEP + NAME_APP + SEP;
     private static final String PDF_NAME = "Observations.pdf";
+
     private static final PDFont TITLE_TEXT_FONT = PDType1Font.HELVETICA_BOLD;
     private static final float TITLE_TEXT_SIZE = 24;
     private static final PDFont SUB_TITLE_TEXT_FONT = PDType1Font.TIMES_ITALIC;
     private static final float SUB_TITLE_TEXT_SIZE = 14;
     private static final PDFont TEXT_FONT = PDType1Font.TIMES_ROMAN;
     private static final float TEXT_SIZE = 16;
+
+    private static final Integer PAGE_START_OFFSET = 50;
+    private static final int TITLE_X_OFFSET = 25;
+    private static final int TEXT_X_OFFSET = 60;
+    private static final float LEADING = 14;
 
     private PDDocument document;
     private PDPageContentStream stream;
@@ -49,25 +55,28 @@ public class PdfExporter {
 
     public boolean exportPdf(Map<String, Map<String, Map<String, Map<String, Integer>>>> allData) {
         try {
-            this.currentLineYValue -= 25;
-            this.addSingleLineText("Observations", 25, this.currentLineYValue, PDType1Font.HELVETICA_BOLD, TITLE_TEXT_SIZE);
+            this.addSingleLineText("Observations", TITLE_X_OFFSET, this.currentLineYValue, PDType1Font.HELVETICA_BOLD, TITLE_TEXT_SIZE);
             this.addSingleLineText("PDF creato il " + " " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")),
                     25, this.currentLineYValue, SUB_TITLE_TEXT_FONT, SUB_TITLE_TEXT_SIZE);
 
-            for (String student: allData.keySet().stream().sorted().collect(Collectors.toList())) {
+            for (String student : allData.keySet().stream().sorted().collect(Collectors.toList())) {
                 this.addSingleLineText(" ", 45, this.currentLineYValue, TITLE_TEXT_FONT, 45);
                 this.addSingleLineText(student, 45, this.currentLineYValue, PDType1Font.TIMES_BOLD, 20);
-                for (String moment: allData.get(student).keySet().stream().sorted().collect(Collectors.toList())) {
+
+                for (String moment : allData.get(student).keySet().stream().sorted().collect(Collectors.toList())) {
                     this.addSingleLineText(" ", 45, this.currentLineYValue, TITLE_TEXT_FONT, TEXT_SIZE);
-                    this.addSingleLineText(moment, 60, this.currentLineYValue, PDType1Font.TIMES_BOLD, 18);
-                    for (String date: allData.get(student).get(moment).keySet().stream().sorted(new DateComparator()).collect(Collectors.toList())) {
+                    this.addSingleLineText(moment, TEXT_X_OFFSET, this.currentLineYValue, PDType1Font.TIMES_BOLD, TEXT_SIZE);
+
+                    for (String date : allData.get(student).get(moment).keySet().stream().sorted(new DateComparator()).collect(Collectors.toList())) {
                         this.addSingleLineText(" ", 45, this.currentLineYValue, TITLE_TEXT_FONT, TEXT_SIZE);
-                        this.addSingleLineText(date, 60 , this.currentLineYValue, TEXT_FONT, TEXT_SIZE);
+                        this.addSingleLineText(date, TEXT_X_OFFSET, this.currentLineYValue, TEXT_FONT, TEXT_SIZE);
+
                         List<String> observationsList = new ArrayList<>();
-                        for (String observation: allData.get(student).get(moment).get(date).keySet().stream().sorted().collect(Collectors.toList())) {
+
+                        for (String observation : allData.get(student).get(moment).get(date).keySet().stream().sorted().collect(Collectors.toList())) {
                             observationsList.add(observation + ": " + allData.get(student).get(moment).get(date).get(observation));
                         }
-                        this.addMultiLineText(observationsList, TEXT_SIZE, 60, this.currentLineYValue, TEXT_FONT, TEXT_SIZE);
+                        this.addMultiLineText(observationsList, LEADING, TEXT_X_OFFSET, this.currentLineYValue, TEXT_FONT, TEXT_SIZE);
                     }
                 }
             }
@@ -93,7 +102,7 @@ public class PdfExporter {
         stream.showText(text);
         stream.endText();
         this.currentLineYValue -= (int) fontSize;
-        stream.moveTo(0,0);
+        stream.moveTo(0, 0);
 
     }
 
@@ -130,7 +139,7 @@ public class PdfExporter {
         this.stream = new PDPageContentStream(this.document, this.page);
         this.pageWidth = (int) this.page.getTrimBox().getWidth();
         this.pageHeight = (int) this.page.getTrimBox().getHeight();
-        this.currentLineYValue = this.pageHeight - 25;
+        this.currentLineYValue = this.pageHeight - PAGE_START_OFFSET;
     }
 
     private boolean isEndOfPage(float offset) {
